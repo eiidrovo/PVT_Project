3fc2bcd84dd8c3ffdf850e9b55fb3b99ee11fa41
eiidrovo <eiidrovo@espol.edu.ec>
2025-06-15 22:47:46 -0500

    PVT-12  PVT-13 FUNCTION FOR THE OIL PRESSURE AND GAS SOLUBILITY

---
+++ pvt/model/pvt.py
@@ -0,0 +1,22 @@
+def pb():
+    """
+    This function retruns the bubble pressure of the oil
+
+    Returns
+    -------
+
+    """
+    PB = "PB"
+    return(PB)
+
+def rs():
+    """
+    This function retruns the bubble pressure of the oil
+
+    Returns
+    -------
+
+    """
+    RS = "GAS SOLUBILITY"
+    return(RS)
+
